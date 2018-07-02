8098b260b62736f1bbafb05d17cc1f41b04dd06f
Soon Kuk Kang <soonkuk@gmail.com>
2018-07-02 14:58:18 +0900

    update config file

--- cli/init_account.sh
+++ cli/init_account.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 bin/brave transaction fund --seed $ROOT_SEED --address $ADDRESS1 --amount 10240
-sleep 3s
+sleep 5s
 bin/brave transaction fund --seed $ROOT_SEED --address $ADDRESS2 --amount 10240
-sleep 3s
+sleep 5s
 bin/brave transaction fund --seed $ROOT_SEED --address $ADDRESS3 --amount 10240
