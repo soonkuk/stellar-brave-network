32be96d150f13d30327522aea3a780753deaab88
Soon Kuk Kang <soonkuk@gmail.com>
2018-07-06 10:22:25 +0900

    modify get_database.sh

--- docker-compose-config/script/get_database.sh
+++ docker-compose-config/script/get_database.sh
@@ -1,11 +1,13 @@
-#!/bin/bash
-field=$1
-if [ ! -d $field ]
-then 
-mkdir $field
-fi
-for i in {1..12}
-do 
+
+        #!/bin/bash
+        field=$1
+        if [ ! -d $field ]
+        then 
+        mkdir $field
+        fi
+        for i in {1..36}
+        do 
+	echo $i":"
 	docker_id=$(docker ps -a | grep "db"$i"_1" | cut -d ' ' -f 1)
 	docker exec -i $docker_id psql -U postgres -d stellar -c "\copy (select * from "$field") to "$field".csv with csv"
 	if [ "$i" -lt 10 ];then
@@ -13,4 +15,5 @@ do
 	else
 	docker cp $docker_id:ledgerheaders.csv $field/"node"$i"_ledgerheaders.csv"
 	fi
-done
+        done
+        
